a5afbd843c92e04b7631ff118ac89a448d42f7eb
Marcus Martinez <marcu0924@gmail.com>
2023-12-28 14:03:45 -0500

    added a global script to change the header

--- javaScripts/main.js
+++ javaScripts/main.js
@@ -9,12 +9,12 @@ class MyHeader extends HTMLElement {
                 background-color: rgb(54, 54, 54);
             }
         </style>
-        <link rel="stylesheet" href="../CSS/headerFooter.css">
+        <link rel="stylesheet" href="CSS/headerFooter.css">
         <title>Rado Network</title>
-        <a href="../home.html"><img id="logo" src="../PNG/rnlogopossibly.png"/></a><div class="container" id="titlebox"><a id="pages1" href="../pages/desktops.html">Desktops</a>
-        <a id="pages1" href="../pages/cases.html">Cases</a><a id="pages1" href="../pages/laptops.html">Laptops</a><a id="pages1" href="../pages/company.html">Company</a>
+        <div id="headerdiv"><a href="home.html"><img id="logo" src="PNG/rnlogopossibly.png"/></a><div class="container" id="titlebox"><a id="pages1" href="desktops.html">Desktops</a>
+        <a id="pages1" href="cases.html">Cases</a><a id="pages1" href="laptops.html">Laptops</a><a id="pages1" href="company.html">Company</a>
         <button id="login">LOGIN</button>
-        </div>
+        </div></div>
     </header>
 
         `
