208275d8284d89784fa3fe646bd7b8f91d272534
Marcus Martinez <marcu0924@gmail.com>
2023-12-31 15:14:30 -0500

    I changed the header again and have to redo some formatting

--- javaScripts/main.js
+++ javaScripts/main.js
@@ -1,21 +1,24 @@
 class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <header>
-            <style>
-            body {
-                margin: 0;
-                font-family: system-ui;
-                background-color: rgb(54, 54, 54);
-            }
-        </style>
-        <link rel="stylesheet" href="CSS/headerFooter.css">
-        <title>Rado Network</title>
-        <div id="headerdiv"><a href="home.html"><img id="logo" src="PNG/rnlogopossibly.png"/></a><div class="container" id="titlebox"><a id="pages1" href="desktops.html">Desktops</a>
-        <a id="pages1" href="cases.html">Cases</a><a id="pages1" href="laptops.html">Laptops</a><a id="pages1" href="company.html">Company</a>
-        <button id="login">LOGIN</button>
-        </div></div>
-    </header>
+        <div class="header">
+        <div class="innerheader">
+            <div class="thing">
+                <div class="logo_container">
+                    <a href="home.html"><img id="logo" src="PNG/rnlogopossibly.png"/></a>
+                </div>
+                <ul class="navigation">
+                    <a href="desktops.html"><li>Desktops</li></a>
+                    <a href="laptops.html"><li>Laptops</li></a>
+                    <a href="cases.html"><li>Cases</li></a>
+                    <a href="company.html"><li>Company</li></a>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+</header>
 
         `
     }
